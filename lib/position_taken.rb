61a4bec3a6581cce8cc7b316d4f5411b654f486f
John Chen <johnchen062094@gmail.com>
2020-04-25 23:13:02 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 board = Array.new(9, " ")
 def position_taken?(board,move)
-  if board[move] == " " or board[move] == ""
+  if board[move] == " " || board[move] == ""
     return board[move]
   else
     return false 
